e1a62abf223ea164fe1df2ae896f73074fd24563
IsadoraMartinez <isadoramartinez123@gmail.com>
2018-11-24 16:26:06 -0500

    Original Commit

---
+++ data2.js
@@ -0,0 +1,158 @@
+var data = {
+	q: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/14.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	w: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/13.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	e: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/12.mp3']
+		}),
+		color: '#3498db'
+	},
+	r: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/11.mp3']
+		}),
+		color: '#9b59b6'
+	},
+		t: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/10.mp3']
+		}),
+		color: '#34495e'
+	},
+	y: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/1.mp3']
+		}),
+		color: '#16a085'
+	},
+	u: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/2.mp3']
+		}),
+		color: '#27ae60'
+	},
+	i: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/3.mp3']
+		}),
+		color: '#2980b9'
+	},
+	o: {
+		sound: new Howl({
+			urls: ['sounds/soundsb/4.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	p: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/5.mp3']
+		}),
+		color: '#2c3e50'
+	},
+	a: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/6.mp3']
+		}),
+		color: '#f1c40f'
+	},
+	s: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/7.mp3']
+		}),
+		color: '#e67e22'
+	},
+		d: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/8.mp3']
+		}),
+		color: '#e74c3c'
+	},
+	f: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/9.mp3']
+		}),
+		color: '#95a5a6'
+	},
+	g: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/15.mp3']
+		}),
+		color: '#f39c12'
+	},
+	h: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/17.mp3']
+		}),
+		color: '#d35400'
+	},
+	j: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/18.mp3']
+		}),
+		color: '#1abc9c'
+	},
+	k: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/19.mp3']
+		}),
+		color: '#2ecc71'
+	},
+	l: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/20.mp3']
+		}),
+		color: '#3498db'
+	},
+	z: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/21.mp3']
+		}),
+		color: '#9b59b6'
+	},
+	x: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/22.mp3']
+		}),
+		color: '#34495e'
+	},
+	c: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/23.mp3']
+		}),
+		color: '#16a085'
+	},
+	v: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/24.mp3']
+		}),
+		color: '#27ae60'
+	},
+	b: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/25.mp3']
+		}),
+		color: '#2980b9'
+	},
+	n: {
+		sound: new Howl({
+			urls: ['sounds/soundsb/26.mp3']
+		}),
+		color: '#8e44ad'
+	},
+	m: {
+		sound: new Howl({
+  		urls: ['sounds/soundsb/27.mp3']
+		}),
+		color: '#2c3e50'
+	}
+}
